676028c9db81a4db41d7d280b1e8a46fca550b09
Mehirion <Mehirion1@gmail.com>
2023-11-08 16:41:09 +0100

    database and schemas setup

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,9 +1,20 @@
+import validators
 
+from fastapi import FastAPI, HTTPException
 
-from fastapi import FastAPI
+from . import schemas
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return "Hi! This is my first python app made with using API :P"
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="your provided URL is not valid")
+    return f"Create database entry for: {url.target_url}"
+
+# @app.get("/")
+# def read_root():
+#     return "Hi! This is my first python app made with using API :P"
